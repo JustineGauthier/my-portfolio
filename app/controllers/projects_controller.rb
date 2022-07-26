18314a272c57c86fe6d0e5bfb5378cf2522e6ee7
Justine Gauthier <101809101+JustineGauthier@users.noreply.github.com>
2022-07-26 13:09:52 +0200

    Index page (#2)
    * add all files and dotenv-rails
    
    * add projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,60 @@
 class ProjectsController < ApplicationController
 end
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy authorized]
+  before_action :authorized, only: %i[edit update destroy]
+
+  def show; end
+
+  def index
+    @projects = Project.all
+    @count = 1
+    @count2 = 1
+    @count3 = 1
+  end
+
+  def new
+    @project = Project.new
+    authorize @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+    authorize @project
+    if @project.save!
+      redirect_to root_path
+    else
+      render :new, notice: 'Oups, il y a eu un petit problème...'
+    end
+  end
+
+  def edit
+    @category_instances = Category.all
+    @category_names = @category_instances.map(&:name)
+    @category_ids = @category_instances.map(&:id)
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to root_path, notice: 'Ce projet à bien été modifié !'
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path, notice: 'Ce projet à bien été supprimé !'
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :url, :creation_date, :category_id, :photo)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def authorized
+    authorize @project
+  end
+end
